5dbb5dcd72f72be25657931438efc4443d3a046e
Alvin Warouw <alvin.warouw@gmail.com>
2020-12-11 11:26:46 +0800

    Artha

---
+++ Client/Services/Interface/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using Artha.Shared.ViewModels;
+using System.Threading.Tasks;
+
+namespace Artha.Client.Services.Interface
+{
+    public interface IAuthService
+    {
+        Task<LoginResult> Login(LoginModel loginModel);
+        Task Logout();
+        Task<RegisterResult> Register(RegisterModel registerModel);
+    }
+}
---
+++ Shared/ViewModels/LoginResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Artha.Shared.ViewModels
+{
+    public class LoginResult
+    {
+        public bool Successful { get; set; }
+        public string Error { get; set; }
+        public string Token { get; set; }
+    }
+}
