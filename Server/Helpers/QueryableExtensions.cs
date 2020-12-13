9516a91672c6cc9f9ae947145ad94b4ee41b827b
awarouw <alvin.warouw@gmail.com>
2020-12-13 14:06:43 +0800

    Artha

---
+++ Server/Helpers/QueryableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Artha.Shared.ViewModels;
+using System.Linq;
+
+namespace Artha.Server.Helpers
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> Paginate<T>(this IQueryable<T> queryable , PaginationViewModel paginationViewModel)
+        {
+            return queryable
+                .Skip((paginationViewModel.Page - 1) * paginationViewModel.RecordsPerPage)
+                .Take(paginationViewModel.RecordsPerPage);
+        }
+    }
+}
