9eedbb7c7a091f44e52706cfe10b04d7760b5447
awarouw <alvin.warouw@gmail.com>
2020-12-13 13:53:02 +0800

    ver1

---
+++ Shared/ViewModels/PaginationViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Artha.Shared.ViewModels
+{
+    public class PaginationViewModel
+    {
+        public int Page { get; set; } = 1;
+        public int RecordsPerPage { get; set; } = 10;
+    }
+}
